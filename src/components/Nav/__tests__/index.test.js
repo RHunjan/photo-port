ab49aeac7a2879a8ce0ab36674c55bcb26bcdf46
ReenaH <Reena_Hunjan@yahoo.com>
2022-09-15 21:00:27 -0400

    code refresh

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,20 +1,20 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
- afterEach(cleanup);
+afterEach(cleanup);
 
 describe('Nav component', () => {
-  // baseline test
   it('renders', () => {
     render(<Nav />);
   });
 
-  // snapshot test
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    // assert value comparison
+    
+    expect(asFragment()).toMatchSnapshot();
   });
 })
 
@@ -24,12 +24,14 @@ describe('emoji is visible', () => {
 
   expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})
+})  
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
+
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
+
 })
